caf017cd322bb291aac335183d4dad7d495d8f95
alyssonaldrin <alysson_cdf@hotmail.com>
2022-05-03 16:52:05 -0300

    resto da atividade

---
+++ Questao3.java
@@ -0,0 +1,5 @@
+class Questao3 {
+  public static void start(String[] args) { //trocamos o metodo 'main' pelo 'start'
+    System.out.print("Terminei a primeira aula com um programa Java!");
+  }
+}
